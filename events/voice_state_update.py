db60834c729c6c81833c6b5535c212bba27f99c2
HiWiSciFi <max.j.hager@gmail.com>
2020-12-08 01:16:08 +0100

    Update voice_state_update.py

--- events/voice_state_update.py
+++ events/voice_state_update.py
@@ -3,8 +3,6 @@ import globals
 @globals.bot.event
 async def on_voice_state_update(member, before, after):
 	if not before.channel and after.channel:
-		role = discord.utils.get(member.guild.roles, name="role name")
 		await member.send(f'you joined a vc')
 	elif before.channel and not after.channel:
-		role = discord.utils.get(member.guild.roles, name="role name")
 		await member.send(f'you left a vc')
