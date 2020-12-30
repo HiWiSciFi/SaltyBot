f4c7fe85796dca4d8b985b14b4a033852fdf834f
HiWiSciFi <max.j.hager@gmail.com>
2020-12-30 21:45:48 +0100

    added extra strings and category support

--- commands/help.py
+++ commands/help.py
@@ -7,8 +7,8 @@ async def command_help(ctx):
 	if ctx.guild.id == 622489511837827072 or ctx.guild.id == 683304302147534889:
 		desc = ''
 		desc += '`help` zeigt diese Nachricht\n'
-		desc += '`cccreate <vc id> <creation name>` Macht den angegebenen vc zu einem "creator"-channel und setzt den Standardnamen für Channel, die von diesem erstellt werden sollen\n'
+		desc += '`cccreate <vc id> <creation name>` Macht den angegebenen vc zu einem creator-channel und setzt den Standardnamen für Channel, die von diesem erstellt werden sollen. Dabei kann der Name des Benutzers verwendet werden indem `°`, oder eine Nummerierung verwendet werden indem `#` eingefügt wird. `\` gibt dabei an, dass das nächste Zeichen normal ausgegeben werden soll.\n'
 		desc += '`ccremove <vc id>` Macht den angegebenen vc wieder zu einem normalen vc\n'
-		desc += '`cclist` Zeigt eine Liste der "creator"-channel\n'
+		desc += '`cclist` Zeigt eine Liste der creator-channel\n'
 		color=0xFF0099
 		await helpers.sendEmbed(ctx, "Help - prefix = ss!", desc, color, 30)
--- events/voice_state_update.py
+++ events/voice_state_update.py
@@ -20,7 +20,34 @@ async def on_voice_state_update(member, before, after):
 
 async def createChannel(member, origChannel):
 	server = origChannel.channel.guild
-	created_channel = await server.create_voice_channel(globals.data[f'creation vcs'][f'{origChannel.channel.id}'])
+	savedchannelname = globals.data[f'creation vcs'][f'{origChannel.channel.id}']
+	channelname = ''
+
+	# get special queues
+	ignore = False
+	for i in range(0, len(savedchannelname)):
+		if not ignore:
+			if savedchannelname[i] == f'\\':
+				ignore = True
+				continue
+			if savedchannelname[i] == f'#':
+				# TODO: implement per creation channel enumeration
+				if not f'channel enumerations' in globals.data:
+					globals.data[f'channel enumerations'] = {}
+				if not f'{origChannel.channel.id}' in globals.data[f'channel enumerations']:
+					globals.data[f'channel enumerations'][f'{origChannel.channel.id}'] = {}
+
+				
+				data.save()
+				continue
+			if savedchannelname[i] == f'°':
+				channelname += member.name
+				continue
+		channelname += savedchannelname[i]
+		ignore = False
+
+
+	created_channel = await server.create_voice_channel(channelname, category=origChannel.channel.category, sync_permissions=True)
 	if not f'created vcs' in globals.data:
 		globals.data[f'created vcs'] = []
 	globals.data[f'created vcs'].append(f'{created_channel.id}')
