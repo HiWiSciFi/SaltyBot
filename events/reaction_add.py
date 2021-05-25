cf4285a02bf498fa58e56cb7a29fd3bb5981c3d0
HiWiSciFi <max.j.hager@gmail.com>
2021-05-25 19:12:45 +0200

    Update reaction_add.py

--- events/reaction_add.py
+++ events/reaction_add.py
@@ -11,36 +11,4 @@ async def on_reaction_add(reaction, user):
 	if f'{reaction.message.channel.id}' in globals.data[f'created tcs'] and f'{reaction.message.channel.guild.id}' in globals.data[f'permitted servers']:
 		# if message was a tc vc config message
 		if f'{reaction.message.id}' in globals.data[f'cconfig msgs'][f'{reaction.message.channel.id}']:
-			# if reaction was lock
-			if reaction.emoji == "🔒":
-				# get tc
-				tc = globals.bot.get_channel(int(reaction.message.channel.id))
-				# get vc
-				vc = globals.bot.get_channel(int(globals.data[f'created tcs'][f'{reaction.message.channel.id}']))
-				# set global permissions
-				await vc.set_permissions(vc.guild.default_role, view_channel=False)
-				# reset reactions
-				await reaction.remove(user)
-				# send channel feedback
-				await reaction.message.edit(embed=helpers.getEmbed("Channel Configuration", f'react with 🔒 to make the voice channel private and with 🔓 to make it public again!\nCurrent status: locked', globals.defaultcolor))
-
-				print(f'Locked channel \"{vc.name}\"')
-
-			# if reaction was unlock
-			elif reaction.emoji == "🔓":
-				# get tc
-				tc = globals.bot.get_channel(int(reaction.message.channel.id))
-				# get vc
-				vc = globals.bot.get_channel(int(globals.data[f'created tcs'][f'{reaction.message.channel.id}']))
-				# set global permissions
-				await vc.set_permissions(vc.guild.default_role, view_channel=True)
-				# reset reactions
-				await reaction.remove(user)
-				# send channel feedback
-				await reaction.message.edit(embed=helpers.getEmbed("Channel Configuration", f'react with 🔒 to make the voice channel private and with 🔓 to make it public again!\nCurrent status: unlocked', globals.defaultcolor))
-
-				print(f'Unlocked channel \"{vc.name}\"')
-
-			# remove unrelevant reactions
-			else:
-				await reaction.remove(user)
+			await reaction.remove(user)
