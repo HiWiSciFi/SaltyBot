2fae7d03ccefc1e3ff642feda5c2ea97b691399c
imurnaili <54451121+imurnaili@users.noreply.github.com>
2021-05-26 17:22:58 +0200

    detection and for the lock emoji

--- events/reaction_add.js
+++ events/reaction_add.js
@@ -1,3 +1,16 @@
 client.on('messageReactionAdd', async (reaction, user) => {
-	
+
+    if(global.data_configMsgs?.[reaction.message.channel] === reaction.message.id){
+        switch (reaction.emoji.toString()) {
+            case "🔒":
+                console.log("🔒");
+                break;
+            case "🔓":
+                console.log("🔓");
+                break;
+        }
+        var reomve = reaction.remove();
+    }
+
+    await reomve;
 });
