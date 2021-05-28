50af5410a0a8d7de19701ed97917acb3baa22b37
imurnaili <54451121+imurnaili@users.noreply.github.com>
2021-05-29 00:20:24 +0200

    locking
    locking and unlocking implemented

--- events/reaction_add.js
+++ events/reaction_add.js
@@ -1,16 +1,32 @@
 client.on('messageReactionAdd', async (reaction, user) => {
+    //exits if the reaction is by the bot
     if(user.id === global.client.user.id) return;
 
+    //if the reaction is on a config Msg
     if(global.data_configMsgs?.[reaction.message.channel] === reaction.message.id){
+        //gets the vc
+        var vc = global.data_createdTcs[reaction.message.channel];
         switch (reaction.emoji.toString()) {
+            //lock vc
             case "🔒":
-                console.log("🔒");
+                console.log(user.username + " locked " + vc.name + " 🔒");
+                //disables VIEW_CHANNEL for all Overwrites
+                for(var permissionOverwrite of vc.permissionOverwrites){
+                    permissionOverwrite[1].update({'VIEW_CHANNEL': false});
+                }
+                //disables VIEW_CHANNEL for @everyone
+                vc.updateOverwrite(vc.guild.roles.everyone, {'VIEW_CHANNEL': false});
                 break;
+
+            //unlock vc
             case "🔓":
-                console.log("🔓");
+                console.log(user.username + " unlocked " + vc.name + " 🔓");
+                //synchonizes the channel with the category
+                vc.lockPermissions();
                 break;
         }
-        var remove = reaction.users.remove(user);
+        
+        //removes the reaction
+        reaction.users.remove(user);
     }
-    await remove;
 });
